b23193d1c7534002beeef0076925e057a1ffb70c
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-07-11 19:20:06 +0530

    fix the spell misake

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -1,7 +1,7 @@
 import {Server} from "socket.io"
 import http from "http"
 import express from "express"
-import Message from "../models/Message.js"  
+import Message from "../models/message.js"  
 import Conservation from "../models/Conservation.js"
 
 const app = express()
